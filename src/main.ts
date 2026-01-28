1b959af5ebf9111f45c218166535a0e155dbaab2
zakaria <baroudamohmmed@gmail.com>
2026-01-28 21:52:06 +0300

    a

--- src/main.ts
+++ src/main.ts
@@ -43,7 +43,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  const port = process.env.PORT || 3001;
+  const port = process.env.PORT || 3002;
 
   // 🔥 المهم
   await app.listen(port, '0.0.0.0');
